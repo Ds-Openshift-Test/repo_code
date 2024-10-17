6062542acee731841a02d9f2cb167ae4a302d311
DS Isai Quiroga <ds.isai.quiroga@gmail.com>
2024-10-17 12:09:25 +0200

    Eliminacion de mala referencia de libreria

--- backend/src/main/java/com/ds/primes_api/PrimesApiApplication.java
+++ backend/src/main/java/com/ds/primes_api/PrimesApiApplication.java
@@ -2,9 +2,6 @@ package com.ds.primes_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-
-import java.util.Objects;
 
 @SpringBootApplication
 public class PrimesApiApplication {
