6b925ad71b288e2740ee635350a930a9014b744f
DS Isai Quiroga <ds.isai.quiroga@gmail.com>
2024-10-29 14:38:51 +0100

    Repared v1.0.1

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://springboot-route-29-project-microservicio.apps.open-shift.digitalsolutions.com:8080'
 };
